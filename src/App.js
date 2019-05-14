988a3ebe1f4fdbbc2747dc9632ddd38af94bc9be
Adam Butterworth <abutterworth@edx.org>
2019-05-14 13:08:12 -0400

    starting point

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+import { default as apiClient } from 'axios';
+import { Input, Button } from '@edx/paragon';
+import siteLanguageOptions from './languages.js';
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      language: 'en',
+      name: null,
+    };
+  }
+
+  save = (event) => {
+    apiClient.post('/dummyurl', {
+      language: this.state.language,
+      name: this.state.name,
+      username: this.props.username,
+    });
+  }
+
+  onLanguageChange = (event) => {
+    this.setState({
+      language: event.target.value,
+    });
+  }
+  onNameChange = (event) => {
+    this.setState({
+      name: event.target.value,
+    });
+  }
+
+  render() {
+    return (
+      <div className="app">
+      
+        <div className="form-group">
+          <label htmlFor="name">Full name</label>
+
+          <Input
+            id="name"
+            type="text"
+            value={this.state.name}
+            onChange={this.onNameChange}
+          />
+        </div>
+
+      
+        <div className="form-group">
+          <label htmlFor="language">Site Language</label>
+
+          <Input
+            id="language"
+            type="select"
+            value={this.state.language}
+            options={siteLanguageOptions}
+            onChange={this.onLanguageChange}
+          />
+        </div>
+
+        <Button
+          className="btn-primary"
+          onClick={this.save}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          Save
+        </Button>
+      </div>
+    );
+  }
 }
+
+App.defaultProps = {
+  username: 'some-username123',
+};
 
 export default App;
---
+++ src/languages.js
@@ -0,0 +1,60 @@
+const siteLanguageList = [
+  {
+    value: 'en',
+    label: 'English',
+  },
+  {
+    value: 'ar',
+    label: 'العربية',
+  },
+  {
+    value: 'ca',
+    label: 'Català',
+  },
+  {
+    value: 'es-419',
+    label: 'Español (Latinoamérica)',
+  },
+  {
+    value: 'fr',
+    label: 'Français',
+  },
+  {
+    value: 'he',
+    label: 'עברית',
+  },
+  {
+    value: 'id',
+    label: 'Bahasa Indonesia',
+  },
+  {
+    value: 'ko-kr',
+    label: '한국어 (대한민국)',
+  },
+  {
+    value: 'pl',
+    label: 'Polski',
+  },
+  {
+    value: 'pt-br',
+    label: 'Português (Brasil)',
+  },
+  {
+    value: 'ru',
+    label: 'Русский',
+  },
+  {
+    value: 'th',
+    label: 'ไทย',
+  },
+  {
+    value: 'uk',
+    label: 'Українська',
+  },
+  {
+    value: 'zh-cn',
+    label: '中文 (简体)',
+  },
+];
+
+export default siteLanguageList;
