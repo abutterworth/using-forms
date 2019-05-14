dfaaf5fb7bf660ed2cb7a571f828adf0310d9a82
Adam Butterworth <abutterworth@edx.org>
2019-05-14 14:36:29 -0400

    we finished

--- src/App.js
+++ src/App.js
@@ -6,14 +6,45 @@ import siteLanguageOptions from './languages.js';
 
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    formData.set('username', this.props.username);
+
+    apiClient.post('/dummyUrl', formData);
   }
 
   render() {
     return (
       <div className="app">
-        <img src={require("./build-this.png")} style={{width:'400px'}} />
+        <form onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="name">Full name</label>
+            <Input
+              type="text"
+              id="name"
+              name="name"
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="lang">Site language</label>
+            <Input
+              type="select"
+              id="lang"
+              name="lang"
+              options={siteLanguageOptions}
+            />
+          </div>
+
+          <Button
+            className="btn-primary"
+            type="submit"
+          >
+            Save
+          </Button>
+        </form>
       </div>
     );
   }
